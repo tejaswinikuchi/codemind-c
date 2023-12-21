df9ad3554d01f4e584837b72cbc465092726a8ba
tejaswinikuchi <147791709+tejaswinikuchi@users.noreply.github.com>
2023-12-21 11:54:42 +0530

    #include<stdio.h> int main() {     int n,i,j,ch;     scanf("%d",&n);     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)         {             printf("%c ",'A'+i);         }         printf(" ");     }
    }

---
+++ Basic_Pattern_1.5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,ch;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%c ",'A'+i);
+        }
+        printf("
+");
+    }
+    
+}
