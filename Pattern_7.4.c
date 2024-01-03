e2ff151b4a6dc2f35d7e1004dc2d553761aeb85f
tejaswinikuchi <147791709+tejaswinikuchi@users.noreply.github.com>
2024-01-03 14:01:20 +0530

    #include<stdio.h> int main() {     int n,i,j,k;     scanf("%d",&n);     for(i=n;i>0;i--)     {         for(j=1;j<=n-i;j++)         {             printf(" ");         }         for(k=1;k<=i;k++)         {             printf("%c ",i+64);         }         printf(" ");     } }

---
+++ Pattern_7.4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j,k;
+    scanf("%d",&n);
+    for(i=n;i>0;i--)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(k=1;k<=i;k++)
+        {
+            printf("%c ",i+64);
+        }
+        printf("
+");
+    }
+}
