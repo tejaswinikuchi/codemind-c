3adfb25b2a6817d258c672d803eb8bd556d26196
tejaswinikuchi <147791709+tejaswinikuchi@users.noreply.github.com>
2024-01-03 16:35:56 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(j%2!=0)printf("1 ");             else printf("0 ");         }         printf(" ");     } }

---
+++ Basic_Pattern_4.3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(j%2!=0)printf("1 ");
+            else printf("0 ");
+        }
+        printf("
+");
+    }
+}
